1afaa4bc0eb1a8dd7be592d8383450b0f87b5859
Pietro Celeste <pietro.celeste@gmail.com>
2023-06-06 16:36:57 +0200

    Added first Predefined action

---
+++ src/Osynapsy/Laravel/Action/Predefined/UploadPhoto.php
@@ -0,0 +1,35 @@
+<?php
+namespace Osynapsy\Laravel\Action\Predefined;
+
+use Osynapsy\Laravel\Action\AbstractAction;
+
+/**
+ * Description of UploadPhoto
+ *
+ * @author peter
+ */
+class UploadPhoto extends AbstractAction
+{
+    public function execute(...$params)
+    {        
+        $fieldId = $params[0][0];
+        $fileFileldId = $fieldId . '_file';
+        $filePath = request()->hasfile($fileFileldId) ? $this->saveFile(request()->file($fileFileldId)) : '';        
+        $this->getResponse()->js(sprintf("document.getElementById('%s').value = '%s'", $fieldId, $filePath));
+        $this->getResponse()->js(sprintf("document.getElementById('%s_file').value = ''", $fieldId));
+        $this->getResponse()->js(sprintf("document.getElementById('%s_preview').innerHTML = '<img src=\"%s\" style=\"max-width: 100%%\">'", $fieldId, $filePath));
+    }
+    
+    protected function saveFile($file)
+    {
+        $repoRoot = $this->getRepoRoot();
+        $result = sprintf('/%s/%s', $repoRoot, $file->getClientOriginalName());
+        $file->move($repoRoot, $file->getClientOriginalName());
+        return $result;        
+    }
+    
+    protected function getRepoRoot()
+    {
+        return 'upload';
+    }
+}
