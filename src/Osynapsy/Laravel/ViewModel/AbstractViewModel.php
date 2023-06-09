e08467778f5aba77662f423078b70ab29c8e5f43
Pietro Celeste <pietro.celeste@gmail.com>
2023-06-09 16:22:11 +0200

    Improve code

--- src/Osynapsy/Laravel/ViewModel/AbstractViewModel.php
+++ src/Osynapsy/Laravel/ViewModel/AbstractViewModel.php
@@ -8,27 +8,40 @@ namespace Osynapsy\Laravel\ViewModel;
  */
 class AbstractViewModel
 {
-    protected $model;
+    protected $laravelModel;
+    public $entityId;
     
     public function getLaravelModel()
     {
-        $this->model;
+        $this->laravelModel;
     }
     
     public function setLaravelModel($model)
     {
-        $this->model = $model;
+        $this->laravelModel = $model;
+    }
+
+    public function getEntityId()
+    {
+        return $this->entityId;
     }
     
-    public function getValue($field)
+    public function getValue($requestField, $dbField = null)
     {
+        $field = empty($dbField) ? $requestField : $dbField;
         $dbValue = $this->model ? $this->model->{$field} : null;
-        return old($field, $dbValue);
+        return old($requestField, $dbValue);
     }
 
     public function doQuery($query, array $parameters = [])
     {
         $rs = \DB::select($query, $parameters);
         return json_decode(json_encode($rs), true);
+    }
+
+    public function setEntityId($routeParameterId, $defaultValue = null)
+    {
+        $id = request()->route($routeParameterId, $defaultValue);
+        $this->entityId = is_null($id) || $id === 'add' ? $defaultValue : $id;
     }
 }
