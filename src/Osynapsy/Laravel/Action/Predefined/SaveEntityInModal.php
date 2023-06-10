f3c4cbb7e2f7997d6a39fb1cb7deae4b5238a9a0
Pietro Celeste <pietro.celeste@gmail.com>
2023-06-10 17:53:40 +0200

    Add predefined action SaveEntityInModal for use with modal view

---
+++ src/Osynapsy/Laravel/Action/Predefined/SaveEntityInModal.php
@@ -0,0 +1,26 @@
+<?php
+namespace Osynapsy\Laravel\Action\Predefined;
+
+
+
+/**
+ * Description of SaveEntity
+ *
+ * @author Pietro Celeste <pietro.celeste@gmail.com>
+ */
+class SaveEntityInModal extends SaveEntity
+{
+    protected function getComponentIdsToRefresh() : array
+    {
+        return [];
+    }
+
+    protected function closeView()
+    {
+        $componentIds = $this->getComponentIdsToRefresh();
+        if (!empty($componentIds)) {
+            $this->getResponse()->js("parent.Osynapsy.refreshComponents(['%s']);", implode("','", $componentIds));
+        }
+        $this->getResponse()->js("parent.Osynapsy.modal.instance.hide();");
+    }
+}
