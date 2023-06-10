b3a208384e1f0c904875066d7877c1dd75d4a7c5
Pietro Celeste <pietro.celeste@gmail.com>
2023-06-10 17:25:09 +0200

    Improve code

--- src/Osynapsy/Laravel/ViewModel/AbstractViewModel.php
+++ src/Osynapsy/Laravel/ViewModel/AbstractViewModel.php
@@ -8,16 +8,29 @@ namespace Osynapsy\Laravel\ViewModel;
  */
 class AbstractViewModel
 {
-    protected $laravelModel;
     public $entityId;
-    
+    protected $laravelModel;
+    protected $fieldMap = [];
+
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    protected function init()
+    {
+    }
+
     public function getLaravelModel()
     {
         return $this->laravelModel;
     }
     
-    public function setLaravelModel($model)
-    {
+    public function setLaravelModel($model, $id = null)
+    {        
+        if (!empty($id)) {
+            $model = $model->find($id);
+        }        
         $this->laravelModel = $model;
     }
 
@@ -26,10 +39,10 @@ class AbstractViewModel
         return $this->entityId;
     }
     
-    public function getValue($requestField, $dbField = null)
+    public function getValue($requestField)
     {
-        $field = empty($dbField) ? $requestField : $dbField;
-        $dbValue = $this->laravelModel ? $this->getLaravelModel()->{$field} : null;
+        $dbField = array_key_exists($requestField, $this->fieldMap) ? $this->fieldMap[$requestField]->getDbName() : $requestField;
+        $dbValue = $this->laravelModel ? $this->getLaravelModel()->{$dbField} : null;
         return old($requestField, $dbValue);
     }
 
@@ -43,5 +56,25 @@ class AbstractViewModel
     {
         $id = request()->route($routeParameterId, $defaultValue);
         $this->entityId = is_null($id) || $id === 'add' ? $defaultValue : $id;
+    }
+
+    public function map($requestField, $dbField, $defaultValue = null)
+    {
+        $modelField = new ViewModelField($requestField, $dbField, $defaultValue);
+        return $this->fieldMap[$modelField->getId()] = $modelField;
+    }
+
+    public function save()
+    {
+        if (empty($this->fieldMap)) {
+            throw new \Exception('No fieldmap exists.');
+        }        
+        $model = $this->getLaravelModel();
+        $result = [];
+        foreach ($this->fieldMap as $field) {
+            $result[] = [$field->getDbName(), $field->getValue()];
+            $model->{$field->getDbName()} = $field->getValue();
+        }        
+        $model->save();
     }
 }
