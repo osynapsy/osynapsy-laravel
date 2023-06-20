536d3dc510a2aa59496ea4b15f8e18d323397482
Pietro Celeste <pietro.celeste@gmail.com>
2023-06-20 09:39:22 +0200

    Moved PRedefined action from package Action to package ViewModel

---
+++ src/Osynapsy/Laravel/ViewModel/Action/DeleteEntity.php
@@ -0,0 +1,32 @@
+<?php
+namespace Osynapsy\Laravel\ViewModel\Action;
+
+use Osynapsy\Laravel\Action\AbstractAction;
+
+/**
+ * Description of DeleteEntity
+ *
+ * @author peter
+ */
+class DeleteEntity extends AbstractAction
+{
+   public function execute(...$params)
+    {
+        try {
+            $this->beforeDelete();
+            $this->getViewModel()->getLaravelModel()->delete();
+            $this->afterDelete();
+        } catch(\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    protected function beforeDelete()
+    {
+    }
+
+    protected function afterDelete()
+    {
+        $this->getResponse()->goto('back');
+    }
+}
---
+++ src/Osynapsy/Laravel/ViewModel/Action/SaveEntity.php
@@ -0,0 +1,79 @@
+<?php
+namespace Osynapsy\Laravel\ViewModel\Action;
+
+use Osynapsy\Laravel\Action\AbstractAction;
+
+/**
+ * Description of SaveEntity
+ *
+ * @author Pietro Celeste <pietro.celeste@gmail.com>
+ */
+class SaveEntity extends AbstractAction
+{
+    public function execute(...$params)
+    {
+        try {
+            $request = request();
+            $mapFields = $this->getViewModel()->getMapFields();
+            $this->validateMapFields($mapFields);
+            $laravelModel = $this->getViewModel()->getLaravelModel();
+            $this->validateLaravelModel($laravelModel);            
+            $response = $this->beforeSave($request, $mapFields);
+            if (!empty($response)) {
+                return $response;
+            }
+            $id = $this->save($laravelModel, $mapFields);            
+            $this->afterSave($request, $id);
+        } catch(\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    protected function validateMapFields($mapFields)
+    {
+        if (empty($mapFields)) {
+            $this->raiseException('No fieldmap exists.');
+        }
+    }
+
+    protected function validateLaravelModel($laravelModel)
+    {
+        if (empty($laravelModel)) {
+            $this->raiseException('No laravel model exists');
+        }
+    }   
+
+    protected function beforeSave($request, $mapFields)
+    {
+        $this->validateUserInput($request, $mapFields);
+    }
+
+    protected function validateUserInput($request, $mapFields)
+    {
+        $rules = [];
+        foreach ($mapFields as $fieldName => $modelField) {
+            $rules[$fieldName] = $modelField->getValidationRules();
+        }
+        $request->validate($rules);
+    }
+
+    protected function save($model, $mapFields)
+    {       
+        foreach ($mapFields as $field) {
+            $model->{$field->getDbName()} = $field->getValue();
+        }
+        $model->save();
+        return $model->id;
+    }
+
+    protected function afterSave($request, $id)
+    {        
+        $redirectUrl = 'back';
+        $urlPart = explode('/', $request->url());
+        if (end($urlPart) === 'add') {
+            $urlPart[array_key_last($urlPart)] = $id;
+            $redirectUrl = implode('/', $urlPart);
+        }
+        $this->getResponse()->goto($redirectUrl);
+    }
+}
