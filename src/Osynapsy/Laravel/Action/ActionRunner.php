9c88c0d40bde2146149cd9eb137480552ad32f46
pietr <pietr@T460S>
2023-06-20 06:52:27 +0200

    bugfix

--- src/Osynapsy/Laravel/Action/ActionRunner.php
+++ src/Osynapsy/Laravel/Action/ActionRunner.php
@@ -9,15 +9,15 @@ namespace Osynapsy\Laravel\Action;
 class ActionRunner
 {
     protected $actions = [];
-    
+
     public function addAction($actionId, $strClass, $viewModel = null)
     {
         if (!is_subclass_of($strClass, AbstractAction::class)) {
-            throw new \Exception(sprintf("Argument #2 (\$strClass) must be of type %s", $strClass));
+            throw new \Exception(sprintf("Argument #2 (\$strClass) must be of type %s", AbstractAction::class));
         }
         $this->actions[$actionId] = [$strClass, $viewModel];
     }
-    
+
     public function getAction($actionId)
     {
         if (!array_key_exists($actionId, $this->actions)) {
@@ -25,26 +25,26 @@ class ActionRunner
         }
         return $this->actions[$actionId];
     }
-    
+
     public function setDefaultAction(callable $action)
     {
         $this->actions['defaultAction'] = $action;
     }
-    
+
     public function execute()
     {
         $actionId = request()->header('Osynapsy-Action');
-        return empty($actionId) ? $this->executeDefaultAction() : $this->executeExternalAction($actionId);                        
+        return empty($actionId) ? $this->executeDefaultAction() : $this->executeExternalAction($actionId);
     }
-    
+
     protected function executeDefaultAction()
     {
         $function = $this->getAction('defaultAction');
         return $function();
     }
-    
+
     protected function executeExternalAction($actionId)
-    {        
+    {
         list($actionClass, $viewModel) = $this->getAction($actionId);
         $actionParams = request()->input('actionParameters') ?? [];
         $action = new $actionClass();
